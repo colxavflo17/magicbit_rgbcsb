5f5b7b2c97620443e998bb399bba609a4c2a83aa
colxavflo17 <73640113+colxavflo17@users.noreply.github.com>
2020-10-29 13:53:39 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
+function A () {
+    magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Blue, ColorEffect.Rotate)
+    magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Red, ColorEffect.None)
+}
+function A2 () {
+    magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Violet, ColorEffect.Rotate)
+    magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Green, ColorEffect.None)
+}
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     if (magicbit.Ultrasonic(DigitalPin.P2) < 10) {
-        magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Red, ColorEffect.Flash)
+        A2()
+        A()
+    } else {
+    	
     }
 })
