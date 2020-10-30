6bad872309c1cede861932751be268192537fd77
colxavflo17 <73640113+colxavflo17@users.noreply.github.com>
2020-10-30 09:50:00 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
+function faireQuelqueChose (_10: number) {
+    basic.showString("Hello!")
+}
 function A () {
     magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Blue, ColorEffect.Rotate)
-    magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Red, ColorEffect.None)
+    magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Black, ColorEffect.None)
 }
 function A2 () {
     magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Violet, ColorEffect.Rotate)
     magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Green, ColorEffect.None)
 }
-basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    if (magicbit.Ultrasonic(DigitalPin.P2) < 10) {
-        A2()
-        A()
-    } else {
-    	
+    if (magicbit.Ultrasonic(DigitalPin.P2) == 7) {
+        magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Indigo, ColorEffect.Flash)
+    }
+    if (magicbit.Ultrasonic(DigitalPin.P2) == 20) {
+        magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Red, ColorEffect.Rotate)
     }
 })
