f3bfcb626905760fd831915afe8b38e98a7aa4e3
colxavflo17 <73640113+colxavflo17@users.noreply.github.com>
2020-10-31 13:54:58 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,11 +9,30 @@ function A2 () {
     magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Violet, ColorEffect.Rotate)
     magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Green, ColorEffect.None)
 }
+OLED.init(128, 64)
+OLED.writeNumNewLine(2)
+OLED.drawLine(
+0,
+0,
+128,
+64
+)
+OLED.drawLine(
+0,
+64,
+128,
+0
+)
 basic.forever(function () {
     if (magicbit.Ultrasonic(DigitalPin.P2) == 7) {
+        OLED.clear()
         magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Indigo, ColorEffect.Flash)
-    }
-    if (magicbit.Ultrasonic(DigitalPin.P2) == 20) {
+        OLED.drawLoading(Math.constrain(magicbit.Ultrasonic(DigitalPin.P2), 0, 100))
+    } else if (magicbit.Ultrasonic(DigitalPin.P2) == 20) {
+        OLED.clear()
         magicbit.RUS_04(RgbUltrasonics.All, RgbColors.Red, ColorEffect.Rotate)
+        OLED.drawLoading(Math.constrain(magicbit.Ultrasonic(DigitalPin.P2), 0, 100))
+    } else {
+    	
     }
 })
